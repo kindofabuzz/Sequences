55c9ecd12b35f41480d56adbf4f51315ef66be53
jason <kindofabuzz@gmail.com>
2022-12-19 15:44:53 -0600

    Initial commit

---
+++ lists_intro.py
@@ -0,0 +1,16 @@
+computer_parts = ["computer",
+                  "monitor",
+                  "keyboard",
+                  "mouse",
+                  "mouse mat"
+                  ]
+
+for part in computer_parts:
+    print(part)
+
+print()
+print(computer_parts[2])
+
+print()
+print(computer_parts[0:3])  #  slicing a list produces another list
+print(computer_parts[-1])
